03241fb27c5c9122e95c18d8d68c8daec3ef6606
Prince Mathew <i.princemathew@gmail.com>
2017-05-23 10:06:47 +0530

    Added the corrected output function
    This function corrects the output decimal value.

--- xor3.py
+++ xor3.py
@@ -1,10 +1,15 @@
 import numpy as np
 
-def nonlin(x, deriv=False):
+def sigmoid(x, deriv=False):
     if (deriv == True):
         return x*(1-x)
 
     return 1/(1+np.exp(-x))
+
+def correct_out(x, in_input=False):
+    if (in_input):
+        return 1-np.round(x)
+    return np.round(x)
 
 X = np.array([
     [0, 0, 0],
@@ -30,23 +35,25 @@ y = np.array([
 
 np.random.seed(1)
 
-syn0 = 2*np.random.random((3, 7)) - 1
-syn1 = 2*np.random.random((7, 1)) - 1
+data_count = len(X)
+
+syn0 = np.random.random((3, data_count))
+syn1 = np.random.random((data_count, 1))
 
 for j in xrange(60000):
     l0 = X
-    l1 = nonlin(np.dot(l0, syn0))
-    l2 = nonlin(np.dot(l1, syn1))
+    l1 = sigmoid(np.dot(l0, syn0))
+    l2 = sigmoid(np.dot(l1, syn1))
     l2_error = y - l2
 
     if (j % 10000) == 0:
         print("Error: "+ str(np.mean(np.abs(l2_error))))
 
-    l2_delta = l2_error * nonlin(l2, deriv=True)
+    l2_delta = l2_error * sigmoid(l2, deriv=True)
 
     l1_error = l2_delta.dot(syn1.T)
 
-    l1_delta = l1_error * nonlin(l1, deriv=True)
+    l1_delta = l1_error * sigmoid(l1, deriv=True)
 
     syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
@@ -55,6 +62,6 @@ print("Output after training", l2)
 
 X1 = input("Enter input data: ")
 L0 = X1
-L1 = nonlin(np.dot(L0, syn0))
-L2 = nonlin(np.dot(L1, syn1))
-print("Output: ", L2)
+L1 = sigmoid(np.dot(L0, syn0))
+L2 = sigmoid(np.dot(L1, syn1))
+print("Output: ", correct_out(L2, X1 in X))
